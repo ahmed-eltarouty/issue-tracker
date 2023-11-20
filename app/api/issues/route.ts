f1ec1165261e1212f108a8cf0760ce6836889e8f
ahmed-eltarouty <ahmed.eltarouty@gmail.com>
2023-11-20 17:48:08 +0400

    implement client side validations

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,11 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import {z} from 'zod'
 import prisma from "@/prisma/client";
-
-const createIssuesSchema = z.object({
-    title: z.string().min(4,'Sorry title can not be empty').max(255),
-    description: z.string().min(5,'description should not be empty')
-})
+import { createIssuesSchema } from "../../validationSchemas";
 
 export async function POST(request: NextRequest){
     const body = await request.json();
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,25 +1,27 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Controller, useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { useState } from "react";
+import {zodResolver} from "@hookform/resolvers/zod"
+import { createIssuesSchema } from "@/app/validationSchemas";
+import {z} from "zod";
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssuesSchema>;
 
 const NewIssuePage = () => {
-    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const { register, control, handleSubmit , formState: {errors}} = useForm<IssueForm>({
+        resolver:zodResolver(createIssuesSchema)
+    });
     const router = useRouter();
     const [error,setError] = useState('')
 
     return (
         <div className="max-w-xl ">
-            {error && <Callout.Root color="red" className="mb-5"> <Callout.Text>{error}</Callout.Text></Callout.Root>}
+            {error && <Callout.Root color="red" as="p" className="mb-5"> <Callout.Text>{error}</Callout.Text></Callout.Root>}
         <form
             className="space-y-3"
             onSubmit={handleSubmit(async (data) => {
@@ -34,6 +36,7 @@ const NewIssuePage = () => {
             <TextField.Root>
                 <TextField.Input placeholder="Title" {...register("title")} />
             </TextField.Root>
+            {errors.title && <Text color="red">{errors.title.message}</Text>}
             <Controller
                 name="description"
                 control={control}
@@ -41,6 +44,7 @@ const NewIssuePage = () => {
                     <SimpleMDE placeholder="Description" {...field} />
                 )}
             />
+            {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
             <Button>Submit New Issue</Button>
         </form>
         </div>
